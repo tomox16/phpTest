7d0addf2834690a04681e1756b3fa30a2c045fb0
三浦友嗣 <>
2020-07-12 20:34:04 +0900

    第3部 - 関数

--- php03.php
+++ php03.php
@@ -32,15 +32,15 @@ echo "\n";
 // 課題4
 function max_array($arr){
     // とりあえず配列の最初の要素を1番大きい値とする
-     $max_number = $arr[0];
-     foreach($arr as $a){
-        if ($max_number < $a) {
-            $max_number = $a;
-        }
-     }
+    $max_number = $arr[0];
+    foreach($arr as $a){
+       if ($max_number < $a) {
+           $max_number = $a;
+       }
+    }
     
-     return $max_number;
-     }
+    return $max_number;
+}
 
 echo max_array($array);
 echo "\n";
